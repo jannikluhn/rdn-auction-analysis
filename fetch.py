4978e2d309807d9eee27dc947eee6300e8a84765
Jannik Luhn <jannik.luhn@brainbot.com>
2017-10-20 15:37:13 +0200

    Some basic stats and more plots

--- fetch.py
+++ fetch.py
@@ -19,9 +19,9 @@ auction_contract = web3.eth.contract(AUCTION_ADDRESS, abi=auction_abi)
 wallet_abi = web3.eth.contract(WALLET_ADDRESS, abi=wallet_abi)
 
 
-def fetch_bids(from_block, to_block):
+def fetch_bids():
     """Get all bid events and return them as a dataframe."""
-    block_range = {'fromBlock': from_block, 'toBlock': to_block}
+    block_range = {'fromBlock': CONTRACT_CREATION_BLOCK}
     # get bid events
     log_filter = auction_contract.on('BidSubmission', block_range)
     events = log_filter.get(only_changes=False)
@@ -53,61 +53,64 @@ def fetch_bids(from_block, to_block):
     return sorted
 
 
-def fetch_txs(from_block, to_block):
-    block_range = {'fromBlock': from_block, 'toBlock': to_block}
-    block_numbers = range(from_block, to_block + 1)
-    txs = []
-    receipts = []
-    for block_number in block_numbers:
-        if (block_number - block_numbers[0]) % 10 == 0:
-            print('fetching blocks {} + 10'.format(block_number))
-        tx_count = web3.eth.getBlockTransactionCount(block_number)
-        for i in range(tx_count):
-            tx = dict(web3.eth.getTransactionFromBlock(block_number, i))
-            if not tx['to'] or not is_same_address(tx['to'], AUCTION_ADDRESS):
-                continue
-            tx['value'] = from_wei(tx['value'], 'ether')
-            receipt = web3.eth.getTransactionReceipt(tx['hash'])
-            txs.append(tx)
-            receipts.append(receipt)
-    tx_df = pd.DataFrame({key: [tx[key] for tx in txs] for key in txs[0].keys()})
-    receipt_df = pd.DataFrame({key: [receipt[key]
-                               for receipt in receipts] for key in receipts[0].keys()})
-    return tx_df, receipt_df
+# def fetch_txs(from_block, to_block):
+#     block_range = {'fromBlock': from_block, 'toBlock': to_block}
+#     block_numbers = range(from_block, to_block + 1)
+#     txs = []
+#     receipts = []
+#     for block_number in block_numbers:
+#         if (block_number - block_numbers[0]) % 10 == 0:
+#             print('fetching blocks {} + 10'.format(block_number))
+#         tx_count = web3.eth.getBlockTransactionCount(block_number)
+#         for i in range(tx_count):
+#             tx = dict(web3.eth.getTransactionFromBlock(block_number, i))
+#             if not tx['to'] or not is_same_address(tx['to'], AUCTION_ADDRESS):
+#                 continue
+#             tx['value'] = from_wei(tx['value'], 'ether')
+#             receipt = web3.eth.getTransactionReceipt(tx['hash'])
+#             txs.append(tx)
+#             receipts.append(receipt)
+#     tx_df = pd.DataFrame({key: [tx[key] for tx in txs] for key in txs[0].keys()})
+#     receipt_df = pd.DataFrame({key: [receipt[key]
+#                                for receipt in receipts] for key in receipts[0].keys()})
+#     return tx_df, receipt_df
 
 
 if __name__ == '__main__':
-    to_block = web3.eth.blockNumber
+    bids = fetch_bids()
+    bids.to_csv(BID_OUTPUT_FILENAME)
 
-    if os.path.exists(BID_OUTPUT_FILENAME):
-        old_bids = pd.DataFrame.from_csv(BID_OUTPUT_FILENAME)
-        from_block = int(max(old_bids['block'] - 10))
-    else:
-        old_bids = None
-        from_block = CONTRACT_CREATION_BLOCK
-    print('fetching bids between {} and {}'.format(from_block, to_block))
-    bids = fetch_bids(from_block, to_block)
-    if old_bids is not None:
-        bids = pd.concat([old_bids, bids], ignore_index=True)
+    # to_block = web3.eth.blockNumber
 
-    if os.path.exists(TX_OUTPUT_FILENAME):
-        old_txs = pd.DataFrame.from_csv(TX_OUTPUT_FILENAME)
-        from_block = int(max(old_txs['blockNumber'] - 10))
-    else:
-        old_txs = None
-        from_block = CONTRACT_CREATION_BLOCK
-    if os.path.exists(RECEIPT_OUTPUT_FILENAME):
-        old_receipts = pd.DataFrame.from_csv(RECEIPT_OUTPUT_FILENAME)
-        from_block = min(from_block, int(max(old_receipts['blockNumber'] - 10)))
-    else:
-        old_receipts = None
-        from_block = CONTRACT_CREATION_BLOCK
-    print('fetching txs between {} and {}'.format(from_block, to_block))
-    txs, receipts = fetch_txs(from_block, to_block)
-    if old_txs is not None:
-        txs = pd.concat([old_txs, txs], ignore_index=True)
-    if old_receipts is not None:
-        receipts = pd.concat([old_receipts, receipts], ignore_index=True)
-    txs.to_csv(TX_OUTPUT_FILENAME)
-    receipts.to_csv(RECEIPT_OUTPUT_FILENAME)
-    print('done')
+    # if os.path.exists(BID_OUTPUT_FILENAME):
+    #     old_bids = pd.DataFrame.from_csv(BID_OUTPUT_FILENAME)
+    #     from_block = int(max(old_bids['block'] - 10))
+    # else:
+    #     old_bids = None
+    #     from_block = CONTRACT_CREATION_BLOCK
+    # print('fetching bids between {} and {}'.format(from_block, to_block))
+    # bids = fetch_bids(from_block, to_block)
+    # if old_bids is not None:
+    #     bids = pd.concat([old_bids, bids], ignore_index=True)
+
+    # if os.path.exists(TX_OUTPUT_FILENAME):
+    #     old_txs = pd.DataFrame.from_csv(TX_OUTPUT_FILENAME)
+    #     from_block = int(max(old_txs['blockNumber'] - 10))
+    # else:
+    #     old_txs = None
+    #     from_block = CONTRACT_CREATION_BLOCK
+    # if os.path.exists(RECEIPT_OUTPUT_FILENAME):
+    #     old_receipts = pd.DataFrame.from_csv(RECEIPT_OUTPUT_FILENAME)
+    #     from_block = min(from_block, int(max(old_receipts['blockNumber'] - 10)))
+    # else:
+    #     old_receipts = None
+    #     from_block = CONTRACT_CREATION_BLOCK
+    # print('fetching txs between {} and {}'.format(from_block, to_block))
+    # txs, receipts = fetch_txs(from_block, to_block)
+    # if old_txs is not None:
+    #     txs = pd.concat([old_txs, txs], ignore_index=True)
+    # if old_receipts is not None:
+    #     receipts = pd.concat([old_receipts, receipts], ignore_index=True)
+    # txs.to_csv(TX_OUTPUT_FILENAME)
+    # receipts.to_csv(RECEIPT_OUTPUT_FILENAME)
+    # print('done')
--- plot.py
+++ plot.py
@@ -15,15 +15,21 @@ def plot_cum_bids(ax, bids):
     time = bids['time']
     bid_amount = bids['amount']
     cum_bid_amount = np.cumsum(bid_amount)
+    cum_bid_number = np.arange(len(bids))
 
     time_scale = 1 / (60 * 60)
     bid_scale = 1 / 1000
     ax.fill_between(time * time_scale, 0, cum_bid_amount * bid_scale, step='pre')
+    ax2 = ax.twinx()
+    ax2.plot(time * time_scale, cum_bid_number, color='C1')
 
     ax.set_xlabel('Time [h]')
-    ax.set_ylabel('Bidded amount [kETH]')
+    ax.set_ylabel('Bid amount [kETH]')
     ax.set_xlim(0, time.iloc[-1] * time_scale)
     ax.set_ylim(0, cum_bid_amount.iloc[-1] * bid_scale)
+
+    ax2.set_ylim(0, len(bids))
+    ax2.set_ylabel('Number of bids')
 
 
 def plot_bid_dist(ax, bids):
@@ -78,6 +84,40 @@ def plot_corr(ax, bids):
     ax2.set_ylabel('Autocorrelation second half [a.u.]')
 
 
+def print_summary(bids):
+    total_amount = bids['amount'].sum()
+    amount_by_sender = bids[['sender', 'amount']].groupby('sender').sum()
+    median_contribution = amount_by_sender.median()
+    average_contribution = amount_by_sender.mean()
+    largest_bids = amount_by_sender.sort_values('amount', ascending=False).iloc[:10]
+    print('total: {}'.format(total_amount))
+    print('median contrib: {}'.format(median_contribution))
+    print('avg contrib: {}'.format(average_contribution))
+    print('largest bids: {}'.format(largest_bids))
+
+
+def plot_lorenz(ax, bids):
+    total_amount = bids['amount'].sum()
+    bidders = bids[['sender', 'amount']].groupby('sender').sum()
+    bidders['percentage'] = bidders['amount'] / bidders['amount'].sum()
+    bidders = bidders.sort_values('percentage')
+
+    lorenz_x = np.arange(len(bidders)) / len(bidders)
+    lorenz_y = np.cumsum(bidders['percentage'])
+    ax.plot(lorenz_x, lorenz_y)
+
+    non_kyc_contrib = bidders[bidders['amount'] <= 2.5]['amount'].sum() / total_amount
+    top_10_percent = bidders.iloc[-int(round(len(bidders) / 10)):]
+    top_10_percent_contrib = top_10_percent['amount'].sum() / total_amount
+    gini = (1 / len(bidders) * lorenz_y).sum() * 2
+    #gini = (1 / len(bidders) * np.linspace(0, 1, len(bidders))).sum() * 2
+
+    print('non KYC contribution: {}%'.format(non_kyc_contrib * 100))
+    print('top 10% contribution: {}%'.format(top_10_percent_contrib * 100))
+    print('Gini coefficient: {}%'.format(gini * 100))
+
+
+
 def plot_failed(ax, txs, receipts):
     merged = pd.merge(txs, receipts, left_index=True, right_index=True)
     failure_details = merged[['gasUsed', 'gas', 'status', 'input', 'value']]
@@ -105,6 +145,16 @@ def plot_learning_bidders(ax, bids, txs, receipts):
     forever_failing = failing_bidders - succeeding_bidders
     import pudb.b
 
+def plot_cum_hist(ax, bids):
+    bidders = bids[['sender', 'amount']].groupby('sender').sum()
+    hist, bin_edges = np.histogram(bidders['amount'], bins=100)
+    width = np.diff(bin_edges)
+    ax.bar(bin_edges[:-1], hist, width=width, bottom=0.001)
+    #ax.plot(bin_edges[:-1], np.cumsum(bidders.sort_values('amount')['amount']))
+    ax2 = ax.twinx()
+    ax2.plot(bin_edges[:-1], np.cumsum(bin_edges[:-1] * hist), color='C1')
+    ax.set_yscale('log')
+
 if __name__ == '__main__':
     bids = pd.DataFrame.from_csv(BID_FILENAME)
     txs = pd.DataFrame.from_csv(TX_FILENAME).set_index('hash')
@@ -112,9 +162,12 @@ if __name__ == '__main__':
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    # plot_cum_bids(ax, bids)
+    plot_cum_bids(ax, bids)
     # plot_bid_dist(ax, bids)
     # plot_corr(ax, bids)
     # plot_failed(ax, txs, receipts)
-    plot_learning_bidders(ax, bids, txs, receipts)
+    # plot_learning_bidders(ax, bids, txs, receipts)
+    # print_summary(bids)
+    # plot_lorenz(ax, bids)
+    # plot_cum_hist(ax, bids)
     plt.show()
